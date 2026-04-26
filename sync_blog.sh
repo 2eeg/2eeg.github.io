c8c9d7491a9b59da55612aa61d070d0750ad7a28
park <park@parkui-MacBookAir.local>
2026-04-26 10:20:22 +0900

    Quartz sync: Apr 26, 2026, 10:20 AM

--- sync_blog.sh
+++ sync_blog.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-# 1. nvm 환경 로드
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# 2. 노드 버전 선택 및 폴더 이동
-nvm use 22
-cd ~/projects/quartz
-
-# 3. 쿼츠 동기화 실행
 npx quartz sync
